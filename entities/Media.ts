76505f270a2459fe548bfdb7282dc98d1ce170ed
Gabriel Azevedo <gpeto91@hotmail.com>
2023-07-12 13:20:18 -0300

    fix: tratar erro no ytdl.getBasicInfo

--- entities/Media.ts
+++ entities/Media.ts
@@ -20,8 +20,39 @@ class Media implements IMedia {
   async downloadVideo(url: string, socketId: string, title: string, artist: string, user?: string): Promise<number> {
     return new Promise(async (resolve, reject) => {
       const audioStream = ytdl(url, { filter: "audioonly" });
-      const metadata = await ytdl.getBasicInfo(url);
-      
+
+      try {
+        const metadata = await ytdl.getBasicInfo(url);
+        const trackTitle = metadata.videoDetails.title.replace(/[&\?:|\\\/|]/gi, "");
+        const filepath = path.resolve(`${this.basePath}/${trackTitle}.mp3`);
+
+        ffmpeg(audioStream)
+          .audioBitrate(128)
+          .audioFrequency(44100)
+          .toFormat("mp3")
+          .save(filepath)
+          .on("end", async () => {
+            await this.queue.loadTrack(`tracks/${trackTitle}.mp3`, { title, artist }, user);
+
+            let queueLength = this.queue.tracks.filter((track) => track.queue).length - 1
+
+            if (this.queue.currentTrack?.queue) {
+              queueLength--;
+            }
+
+            resolve(queueLength >= 0 ? queueLength : 0);
+          })
+          .on("error", (err) => {
+            console.log(`Erro no título:`, trackTitle);
+            console.log(err);
+            reject(`Não foi possível baixar o link fornecido`);
+          })
+      } catch (err) {
+        reject(err);
+        return;
+      }
+
+
       // const category = metadata.videoDetails.category;
 
       //TODO verificar necessidade da validação por categoria
@@ -29,34 +60,6 @@ class Media implements IMedia {
         reject("Verifique se o link fornecido é uma música");
         return;
       } */
-
-      const trackTitle = metadata.videoDetails.title.replace(/[&\?:|\\\/|]/gi, "");
-      const filepath = path.resolve(`${this.basePath}/${trackTitle}.mp3`);
-
-      ffmpeg(audioStream)
-        .audioBitrate(128)
-        .audioFrequency(44100)
-        .toFormat("mp3")
-        .outputOptions(
-          '-metadata', `title=${trackTitle}`
-        )
-        .save(filepath)
-        .on("end", async () => {
-          await this.queue.loadTrack(`tracks/${trackTitle}.mp3`, { title, artist }, user);
-
-          let queueLength = this.queue.tracks.filter((track) => track.queue).length - 1
-
-          if (this.queue.currentTrack?.queue) {
-            queueLength--;
-          }
-          
-          resolve(queueLength >= 0 ? queueLength : 0);
-        })
-        .on("error", (err) => {
-          console.log(`Erro no título:`, trackTitle);
-          console.log(err);
-          reject(`Não foi possível baixar o link fornecido`);
-        })
     });
   }
 }
